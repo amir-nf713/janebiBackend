d001c87036b68d99d95d123d15f0d502adcecefe
Amirreza <amir.nf713@gmail.com>
2025-02-20 17:59:41 +0330

    n

--- admin/Radmin.js
+++ admin/Radmin.js
@@ -6,7 +6,7 @@ const adminRouter = require("./admin")
 
 router.get("/", adminRouter.getAdmin)
 router.get('/add/admin/to/data/base/:number', adminRouter.addAdmin)
-'/api/admin/add/admin/to/data/base/:09336230914'
+
 
 
 module.exports = router;
--- admin/admin.js
+++ admin/admin.js
@@ -1,47 +1,54 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 mongoose
    .connect("mongodb://localhost:27017/Savecode")
-   .then(() => console.log("connect to Admin"))
-   .catch(() => console.log("cant connect to Admin"))
+   .then(() => console.log("Connected to MongoDB"))
+   .catch((error) => console.log("Error connecting to MongoDB:", error));
 
-   const adminSchema = new mongoose.Schema({
-      phoneNumber : {type: String, unique: true},
-    })
+const adminSchema = new mongoose.Schema({
+  phoneNumber: { type: String, unique: true },
+});
 
-   const Admin = mongoose.model('admin', adminSchema)
+const Admin = mongoose.model('admin', adminSchema);
 
-   exports.getAdmin = async (req,res) => {
-         try {
-            res.json({
-                data : await Admin.find()
-            })
-         } catch (error) {
-            res.json({
-                massage: "you have err"
-            })
-         }
+exports.getAdmin = async (req, res) => {
+  try {
+    const admins = await Admin.find();
+    res.json({
+      data: admins
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error fetching admins",
+      error: error.message
+    });
+  }
+};
+
+exports.addAdmin = async (req, res) => {
+  try {
+    const phoneNumber = req.params.number; // دریافت شماره تلفن از پارامتر URL
+    const admin = await Admin.findOne({ phoneNumber });
+
+    if (admin) {
+      return res.json({ message: "Admin is already in database" });
     }
 
-    exports.addAdmin = async (req,res) => {
-        try {
-          const phoneNumber = req.params.number
-          const admin = await Admin.findOne({phoneNumber})
-          if (admin) {
-            return res.json({massage: "admin is in database"})
-          }
-          const newAdmin = new Admin({
-            phoneNumber: addAdmin
-          })
+    const newAdmin = new Admin({
+      phoneNumber: phoneNumber // شماره تلفن دریافتی
+    });
 
-          const result = await newAdmin.save()
+    const result = await newAdmin.save();
 
-          res.json({ data: result})
-           
+    res.status(201).json({
+      message: "Admin added successfully",
+      data: result
+    });
 
-        } catch (error) {
-           res.json({
-               massage: "you have err"
-           })
-        }
-   }
+  } catch (error) {
+    res.status(500).json({
+      message: "Error adding admin",
+      error: error.message
+    });
+  }
+};
