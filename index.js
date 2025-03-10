da4d2bbdf68bbf33cb8b033b9614ca5614230b97
Amirreza <amir.nf713@gmail.com>
2025-03-10 14:02:28 +0330

    m

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ app.use(bodyParser.json({ limit: '10mb' })); // اینجا حجم درخواست
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
  
 
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin", "janebi-speed.ir");
+const allowedOrigins = ['http://janebi-speed.ir', 'https://janebi-speed.ir'];
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
-  })
-  
+});
+
 app.use(express.json());
-const cors = require("cors");
-app.use(cors());
-
-
 
 
 
