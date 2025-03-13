d7ccfdc8f6659b96c63db35adb1f5d69e2c41ad4
Amirreza <amir.nf713@gmail.com>
2025-03-13 23:00:29 +0330

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
         res.setHeader("Access-Control-Allow-Origin", origin); // اجازه دسترسی به دامنه‌های مشخص شده
     }
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
