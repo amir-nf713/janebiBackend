039ccb02177de18810f928be047aad123af59402
Amirreza <amir.nf713@gmail.com>
2025-05-07 00:28:26 +0330

    modified:   mongo.js

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const apiKey = {
-    db: "AsAz1323@@:AsAz1323@13!!!@1323@131440@172.17.0.2:27017" //"172.17.0.2:27017"
-}
+    db: "AsAz1323@@:AsAz1323@13!!!@1323@131440@172.17.0.2:27017" // اتصال به MongoDB با username و password
+};
 
 module.exports = apiKey;
