4f137262a4773a9e5532b26ae742b5220165f771
Amirreza <amir.nf713@gmail.com>
2025-05-07 01:30:34 +0330

    Changes to be committed: 	modified:   mongo.js
    Changes not staged for commit:
    	modified:   mongo.js

--- mongo.js
+++ mongo.js
@@ -1,5 +1,6 @@
 const apiKey = {
-  db: "AsAz1323%40%40:AsAz1323%4013%21%21%21@172.17.0.2:27017"
+  db: "AsAz1323%40%40:AsAz1323%4013%21%21%21@mongodb:27017"  // توجه به نام سرویس MongoDB
 };
 
 module.exports = apiKey;
+
