c29e52de7134175753f6164188c473330a1354ed
Amirreza <amir.nf713@gmail.com>
2025-05-07 01:30:51 +0330

    modified:   mongo.js

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const apiKey = {
-  db: "AsAz1323%40%40:AsAz1323%4013%21%21%21@mongodb:27017"  // توجه به نام سرویس MongoDB
+  db: "AsAz1323%40%40:AsAz1323%4013%21%21%21@185.190.39.92:27017"  // استفاده از IP واقعی سرور
 };
 
 module.exports = apiKey;
