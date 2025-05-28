d2a292dd8bd2051e24d0582efcca766b07786c02
Amirreza <amir.nf713@gmail.com>
2025-05-28 05:08:34 +0330

    modified:   Signup/RsendSMS.js 	modified:   Signup/sendSMS.js

--- Signup/RsendSMS.js
+++ Signup/RsendSMS.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const SendSMS = require('./sendSMS')
 
 router.post('/smsSend', SendSMS.sendSMS)
+router.post('/smsSendq', SendSMS.sendSMSbybas)
 
 module.exports = router;
 
--- Signup/sendSMS.js
+++ Signup/sendSMS.js
@@ -1,11 +1,9 @@
-const soap = require('soap');
-const mongoose = require('mongoose');
+const soap = require("soap");
+const mongoose = require("mongoose");
 // const { db } = require('../mongo');  // وارد کردن apiKey.db از فایل mongo.js
-const { username, password, host, port } = require('../mongo');
+const { username, password, host, port } = require("../mongo");
 
-const wsdlUrl = 'http://87.248.137.75/webservice/send.php?wsdl';
-
-
+const wsdlUrl = "http://87.248.137.75/webservice/send.php?wsdl";
 
 // URL-Encode برای کاراکترهای خاص
 const encodedUsername = encodeURIComponent(username);
@@ -13,22 +11,25 @@ const encodedPassword = encodeURIComponent(password);
 
 const uri = `mongodb://${encodedUsername}:${encodedPassword}@${host}:${port}/Savecode?authSource=admin`;
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('✅ Connected to MongoDB');
-}).catch(err => {
-  console.error('❌ Could not connect to MongoDB:', err.message);
-});
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("❌ Could not connect to MongoDB:", err.message);
+  });
 
 const seveCode = new mongoose.Schema({
   number: String,
   code: String,
-  expiresAt: { type: Date, default: Date.now, index: { expires: '2m' } }
+  expiresAt: { type: Date, default: Date.now, index: { expires: "2m" } },
 });
 
-const SaveCode = mongoose.model('savecode', seveCode);
+const SaveCode = mongoose.model("savecode", seveCode);
 
 exports.sendSMS = async (req, res) => {
   const randomCode = Math.floor(Math.random() * 90000) + 10000;
@@ -41,13 +42,18 @@ exports.sendSMS = async (req, res) => {
   const sendSmsInformation = {
     formNum: "9981802432",
     toNum: [number],
-    content: `اسپیدی عزیز خوش آمدی
-      کد ورود شما : ${randomCode}
-      لغو11
+    content: `
+                              سلام!
+      کد ورود به حساب شما: ${randomCode}
+      برای ادامه، این کد رو توی سایت وارد کن.  
+      این کد تا 2 دقیقه فعاله و مخصوص شماست—لطفاً پیش خودتون نگهش دارین. 
+
+www.janebi-speed.ir
+لغو11
     `,
     type: "1",
     username: "alirezamahmooudi94",
-    password: "2981228935"
+    password: "2981228935",
   };
 
   try {
@@ -72,7 +78,51 @@ exports.sendSMS = async (req, res) => {
       massage: "Done successfully",
     });
   } catch (err) {
-    console.error('خطا در ارسال پیامک یا ذخیره‌سازی کد:', err);
-    return res.status(500).send('خطا در ارسال پیامک یا ذخیره‌سازی کد');
+    console.error("خطا در ارسال پیامک یا ذخیره‌سازی کد:", err);
+    return res.status(500).send("خطا در ارسال پیامک یا ذخیره‌سازی کد");
+  }
+};
+
+
+exports.sendSMSbybas = async (req, res) => {
+  
+  const { number , name} = req.body;
+
+  
+
+  const sendSmsInformation = {
+    formNum: "9981802432",
+    toNum: [number],
+    content: `
+     کاربر عزیز ${name} یک سفارش برای شما ثبت شده و درحال بررسی میباشد
+
+www.janebi-speed.ir
+لغو11
+    `,
+    type: "1",
+    username: "alirezamahmooudi94",
+    password: "2981228935",
+  };
+
+  try {
+    // ایجاد کلاینت SOAP
+    const client = await soap.createClientAsync(wsdlUrl);
+
+    // فراخوانی متد SendSMS
+    const result = await client.SendSMSAsync(sendSmsInformation);
+
+    // ذخیره‌سازی کد در MongoDB
+    
+   
+    
+    
+
+    res.json({
+      result: result,
+      massage: "Done successfully",
+    });
+  } catch (err) {
+    console.error("خطا در ارسال پیامک یا ذخیره‌سازی کد:", err);
+    return res.status(500).send("خطا در ارسال پیامک یا ذخیره‌سازی کد");
   }
 };
